9e3c4df5c93ab152eadab6543539bb49edbd530d
acican <acican@gmail.com>
2018-11-19 17:01:31 +0200

    Update lcd3.ino

--- lcd3.ino
+++ lcd3.ino
@@ -1,13 +1,6 @@
 //Sample using LiquidCrystal library
 #include <LiquidCrystal.h>
 
-/*******************************************************
-
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
-
-********************************************************/
-
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
